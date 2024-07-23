dc79749d644a245624d3f6f79de07dc2ec741b9f
wallace <wallace_wolv@hotmail.com>
2024-07-23 02:28:37 -0300

    feat: Configurando uso do package core

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from 'react-native'
+import { somar } from '@barba/core'
 
 export default function App() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>App</Text>
+            <Text>App {somar(50, 30)}</Text>
         </View>
     )
 }
