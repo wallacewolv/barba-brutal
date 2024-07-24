121582b1af2fe0d1fa140ca4d438b8099ee3d92d
wallace <wallace_wolv@hotmail.com>
2024-07-24 00:34:29 -0300

    feat: Adicionando ContainerCOmBackground e NossosServicos na Landing

--- apps/frontend/src/app/(paginas)/(landing)/page.tsx
+++ apps/frontend/src/app/(paginas)/(landing)/page.tsx
@@ -1,11 +1,16 @@
 'use-client'
 
 import TituloSlogan from "@/components/landing/TituloSlogan"
+import NossosServicos from "@/components/servico/NossosServicos"
+import ContainerComBackground from "@/components/shared/ContainerComBackground"
 
 export default function Landing() {
     return (
         <div>
             <TituloSlogan />
+            <ContainerComBackground imagem="/banners/servicos.webp">
+                <NossosServicos />
+            </ContainerComBackground>
         </div>
     )
 }
